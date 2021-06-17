96ee6a928a247c04ca49759ce20f9389ea95de3c
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-06-17 17:04:00 +0300

    A simple shared preferences example was created with Flutter web.web interface is being developed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shared_pref_ex/SayfaA.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
@@ -40,70 +41,127 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-          colors: [Colors.blueGrey,Colors.purple],
-        )),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.all(100),
-
-              width: (MediaQuery.of(context).size.width / 5)*2,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0,5),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 100),
-                    child: Text("Login",style: TextStyle(fontSize: 50,color: Colors.black26,),textAlign: TextAlign.left,),
-                  ),
-                  Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-                      child: TextField()),
-                  Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-                      child: TextField()),
-                  Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-                      child: TextField()),
-                  ElevatedButton(
-                      onPressed: () {
-                        dataLog();
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => SayfaA()));
-                      },
-                      child: Text("İkinci Sayfa"))
-                ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/background.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
-            Container(
-              child: Image.asset("images/note.png",fit: BoxFit.cover,width: MediaQuery.of(context).size.width/3,),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [Colors.blueGrey.withOpacity(0.5),Colors.purple.withOpacity(0.5)],
+                )),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(100),
+                  width: (MediaQuery.of(context).size.width / 5)*2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0,5),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 100),
+                        child: Text("Login",style: TextStyle(fontSize: 50,color: Colors.black26,),textAlign: TextAlign.left,),
+                      ),
+                      Container(
+                          padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                          child: TextField()),
+                      Container(
+                          padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                          child: TextField()),
+                      Container(
+                          padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                          child: TextField()),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 100),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 100,
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                                child: ElevatedButton(
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(horizontal: 20),
+                                    child: Row(
+                                      children: [
+                                        Icon(FontAwesome5.google),
+                                        SizedBox(width: 10,),
+                                        Text("Google ile Giriş Yap")
+                                      ],
+                                    ),
+                                  ),
+                                  onPressed: (){},
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 20,),
+                            Expanded(
+                              child: Container(
+                                height: 100,
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                                child: ElevatedButton(
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(horizontal: 20),
+                                    child: Row(
+                                      children: [
+                                        Icon(FontAwesome5.google),
+                                        SizedBox(width: 10,),
+                                        Text("Google ile Giriş Yap")
+                                      ],
+                                    ),
+                                  ),
+                                  onPressed: (){},
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            dataLog();
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => SayfaA()));
+                          },
+                          child: Text("İkinci Sayfa"))
+                    ],
+                  ),
+                ),
+               /* Container(
+                  child: Image.asset("images/note.png",fit: BoxFit.cover,width: MediaQuery.of(context).size.width/3,),
+                ),*/
+              ],
             ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ],
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

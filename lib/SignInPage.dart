da587df8de56ba74f10a929ddf251b73fed6e035
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-06-28 07:32:39 +0300

    Added login simulation.

--- lib/SignInPage.dart
+++ lib/SignInPage.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shared_pref_ex/AppPage.dart';
 import 'package:flutter_shared_pref_ex/CustomTextfield.dart';
 import 'package:flutter_shared_pref_ex/SignUpPage.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
@@ -13,6 +14,24 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+
+  String userName, password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> loginControl(BuildContext context) async {
+    _formKey.currentState.save();
+
+    if (userName == 'yigit' && password == '123') {
+      print("Giriş Başarılı");
+      Navigator.push(context,
+          MaterialPageRoute(
+              builder: (context) => AppPage()));
+    } else {
+      print("Giriş Başarısız");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +46,22 @@ class _SignInPageState extends State<SignInPage> {
               ),
             ),
             Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height,
               decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.bottomLeft,
                     end: Alignment.topRight,
-                    colors: [Colors.blueGrey.withOpacity(0.5),Colors.purple.withOpacity(0.5)],
+                    colors: [
+                      Colors.blueGrey.withOpacity(0.5),
+                      Colors.purple.withOpacity(0.5)
+                    ],
                   )),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -45,7 +73,10 @@ class _SignInPageState extends State<SignInPage> {
                     ),
                     margin: EdgeInsets.all(100),
                     padding: EdgeInsets.symmetric(horizontal: 100),
-                    width: (MediaQuery.of(context).size.width / 5)*2,
+                    width: (MediaQuery
+                        .of(context)
+                        .size
+                        .width / 5) * 2,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -54,7 +85,7 @@ class _SignInPageState extends State<SignInPage> {
                           color: Colors.black.withOpacity(0.2),
                           spreadRadius: 5,
                           blurRadius: 7,
-                          offset: Offset(0,5),
+                          offset: Offset(0, 5),
                         ),
                       ],
                     ),
@@ -63,28 +94,77 @@ class _SignInPageState extends State<SignInPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Container(
-                          child: Text("Sign In",style: TextStyle(fontSize: 15.sp,color: Colors.black,fontWeight: FontWeight.bold),textAlign: TextAlign.left,),
+                          child: Text("Sign In", style: TextStyle(fontSize: 15
+                              .sp, color: Colors.black, fontWeight: FontWeight
+                              .bold), textAlign: TextAlign.left,),
                         ),
                         Container(
                           margin: EdgeInsets.only(top: 10),
-                          child: Text("This is an example of a sign in page written in Flutter.",style: TextStyle(fontSize: 5.sp,fontWeight: FontWeight.w200,color: Colors.grey,),textAlign: TextAlign.left,),
+                          child: Text(
+                            "This is an example of a sign in page written in Flutter.",
+                            style: TextStyle(fontSize: 5.sp,
+                              fontWeight: FontWeight.w200,
+                              color: Colors.grey,), textAlign: TextAlign.left,),
                         ),
                         SizedBox(height: 50,),
-                        Container(
-                            padding:
-                            EdgeInsets.symmetric(vertical: 10),
-                            child: CustomTextField(
-                              baslik: "E-Mail",
-                              isLighter: true,
+                        Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                Container(
+                                    padding:
+                                    EdgeInsets.symmetric(vertical: 10),
+                                    child: CustomTextField(
+                                      baslik: "E-Mail",
+                                      isLighter: true,
+                                      onSaved: (input) => userName = input,
+                                    )),
+                                Container(
+                                    padding:
+                                    EdgeInsets.symmetric(vertical: 10),
+                                    child: CustomTextField(
+                                      baslik: "Password",
+                                      isLighter: true,
+                                      obsecure: true,
+                                      onSaved: (input) => password = input,
+                                    )),
+                              ],
                             )),
+                        SizedBox(height: 1.h,),
                         Container(
-                            padding:
-                            EdgeInsets.symmetric(vertical: 10),
-                            child: CustomTextField(
-                              baslik: "Password",
-                              isLighter: true,
-                              obsecure: true,
-                            )),
+                          height: 60,
+                          margin: EdgeInsets.symmetric(horizontal: 60),
+                          child: ElevatedButton(
+                            child: Container(
+                              //color: Colors.green,
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Sign In", style: TextStyle(
+                                      fontSize: 6.sp,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.purple.shade900,
+                              side: BorderSide(
+                                width: 2, color: Colors.purple.shade900,),
+                              alignment: Alignment.center,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                side: BorderSide(
+                                  width: 2, color: Colors.purple.shade900,),
+                              ),
+                            ),
+                            onPressed: () {
+                             loginControl(context);
+                            },
+                          ),
+                        ),
                         SizedBox(height: 2.h,),
                         Container(
                           padding: EdgeInsets.symmetric(horizontal: 10.w),
@@ -92,12 +172,16 @@ class _SignInPageState extends State<SignInPage> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
-                              Expanded(child: Divider(color: Colors.grey,height: 2,)),
+                              Expanded(child: Divider(
+                                color: Colors.grey, height: 2,)),
                               Container(
                                 margin: EdgeInsets.symmetric(horizontal: 1.w),
-                                child: Center(child: Text("OR",style: TextStyle(fontSize: 5.sp,color: Colors.grey),)),
+                                child: Center(child: Text("OR",
+                                  style: TextStyle(
+                                      fontSize: 5.sp, color: Colors.grey),)),
                               ),
-                              Expanded(child: Divider(color: Colors.grey,height: 2,))
+                              Expanded(child: Divider(
+                                color: Colors.grey, height: 2,))
                             ],
                           ),
                         ),
@@ -115,27 +199,34 @@ class _SignInPageState extends State<SignInPage> {
                                     child: Container(
                                       //color: Colors.green,
                                       alignment: Alignment.center,
-                                      padding: EdgeInsets.symmetric(horizontal: 20),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 20),
                                       child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .center,
                                         children: [
-                                          Icon(FontAwesome5.google,size: 2.w,color: Colors.purple.shade900,),
+                                          Icon(FontAwesome5.google, size: 2.w,
+                                            color: Colors.purple.shade900,),
                                           SizedBox(width: 10,),
-                                          Text("Sign In With Google",style: TextStyle(fontSize: 4.sp,color: Colors.purple.shade900),)
+                                          Text("Sign In With Google",
+                                            style: TextStyle(fontSize: 4.sp,
+                                                color: Colors.purple.shade900),)
                                         ],
                                       ),
                                     ),
                                     style: ElevatedButton.styleFrom(
                                       //primary: Colors.pink,
                                       primary: Colors.white,
-                                      side: BorderSide(width: 2,color: Colors.purple.shade900,),
+                                      side: BorderSide(width: 2,
+                                        color: Colors.purple.shade900,),
                                       alignment: Alignment.center,
                                       shape: RoundedRectangleBorder(
                                         borderRadius: BorderRadius.circular(15),
-                                        side: BorderSide(width: 2,color: Colors.purple.shade900,),
+                                        side: BorderSide(width: 2,
+                                          color: Colors.purple.shade900,),
                                       ),
                                     ),
-                                    onPressed: (){},
+                                    onPressed: () {},
                                   ),
                                 ),
                               ),
@@ -145,25 +236,32 @@ class _SignInPageState extends State<SignInPage> {
                                   height: 60,
                                   child: ElevatedButton(
                                     child: Container(
-                                      margin: EdgeInsets.symmetric(horizontal: 20),
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: 20),
                                       child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .center,
                                         children: [
-                                          Icon(FontAwesome5.apple,size: 2.w,color: Colors.purple.shade900,),
+                                          Icon(FontAwesome5.apple, size: 2.w,
+                                            color: Colors.purple.shade900,),
                                           SizedBox(width: 10,),
-                                          Text("Sign In With Apple",style: TextStyle(fontSize: 4.sp,color: Colors.purple.shade900),)
+                                          Text("Sign In With Apple",
+                                            style: TextStyle(fontSize: 4.sp,
+                                                color: Colors.purple.shade900),)
                                         ],
                                       ),
                                     ),
-                                    onPressed: (){},
+                                    onPressed: () {},
                                     style: ElevatedButton.styleFrom(
                                       //primary: Colors.pink,
                                       primary: Colors.white,
-                                      side: BorderSide(width: 2,color: Colors.purple.shade900,),
+                                      side: BorderSide(width: 2,
+                                        color: Colors.purple.shade900,),
                                       alignment: Alignment.center,
                                       shape: RoundedRectangleBorder(
                                         borderRadius: BorderRadius.circular(15),
-                                        side: BorderSide(width: 2,color: Colors.purple.shade900,),
+                                        side: BorderSide(width: 2,
+                                          color: Colors.purple.shade900,),
                                       ),
                                     ),
                                   ),
@@ -180,15 +278,19 @@ class _SignInPageState extends State<SignInPage> {
                             text: TextSpan(
                                 children: <TextSpan>[
                                   TextSpan(
-                                    text: "Don't have an account?" ,
+                                    text: "Don't have an account?",
                                     style: TextStyle(color: Colors.grey),
                                   ),
                                   TextSpan(
                                     text: 'Sign Up!',
-                                    style: TextStyle(color: Colors.purple.shade900),
+                                    style: TextStyle(
+                                        color: Colors.purple.shade900),
                                     recognizer: TapGestureRecognizer()
-                                      ..onTap = (){
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                                      ..onTap = () {
+                                        Navigator.push(context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SignUpPage()));
                                       },
                                   ),
                                 ]
